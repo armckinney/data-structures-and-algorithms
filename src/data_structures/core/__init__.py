11c1066d1144958c978e48f46efa3a051c76a7f0
Andrew McKinney <andrew.mckinney@vumc.org>
2024-02-19 21:28:04 +0000

    tmp checkin

--- src/data_structures/core/__init__.py
+++ src/data_structures/core/__init__.py
@@ -1,5 +1,6 @@
 from .data_structure import DataStructure
 from .linear_structure import LinearStructure
+from .mapped_structure import MappedStructure
 from .node import Node
 
-__all__ = ["Node", "LinearStructure", "DataStructure"]
+__all__ = ["Node", "LinearStructure", "DataStructure", "MappedStructure"]
--- tests/data_structures/hash_table/test_hash_table.py
+++ tests/data_structures/hash_table/test_hash_table.py
@@ -1,6 +1,38 @@
 from data_structures import HashTable
 
+TEST_DICT = {"a": 1, "b": 2, "c": 3}
+
 
 def test_can_create() -> None:
     # Assemble / Act / Assert
     HashTable()
+
+
+def test_can_create_different_size() -> None:
+    # Assemble
+    size = 5
+
+    # Act
+    ht = HashTable(size)
+
+    # Assert
+    assert ht.size == size
+
+
+def test_creates_different_size_buckets() -> None:
+    # Assemble
+    size = 5
+
+    # Act
+    ht = HashTable(size)
+
+    # Assert
+    assert len(ht._buckets) == size
+
+
+def test_can_create_from_dict() -> None:
+    # Assemble / Act
+    ht = HashTable.from_dict(TEST_DICT, 1)
+
+    # Assert
+    assert ht.to_dict() == TEST_DICT
